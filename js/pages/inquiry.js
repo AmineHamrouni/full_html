1dd516eb09597480ce570056fe7a09489a27e8ae
Amine <hamrouni.amin@gmail.com>
2017-02-19 16:08:01 +0300

    inquiry

--- js/pages/inquiry.js
+++ js/pages/inquiry.js
@@ -1,6 +1,17 @@
 $(document).ready(function () {
     $("#form").steps({
         bodyTag: "fieldset",
+        errorLabelContainer: "#messageBox",
+        /* Labels */
+        labels: {
+            cancel: "إلغاء",
+            current: "المرحلة الحالية:",
+            pagination: "Pagination",
+            finish: "إنهاء",
+            next: "التالي",
+            previous: "السابق",
+            loading: "تحميل ..."
+        },
         onStepChanging: function (event, currentIndex, newIndex) {
             // Always allow going backward even if the current step contains invalid fields!
             if (currentIndex > newIndex) {
@@ -55,21 +66,76 @@ $(document).ready(function () {
             form.submit();
         }
     }).validate({
-        errorPlacement: function (error, element) {
-            element.before(error);
-        },
+//        errorElement: "em",
+//        success: function (label) {
+//            label.text("ok!").parent().addClass("has-success");
+//        },
+//        error: function (label) {
+//            label.text("dwdwdwdwdw!").parent().addClass("has-error");
+//        },
         rules: {
             confirm: {
                 equalTo: "#password"
+            },
+            vacationType: {
+                required: true
+            },
+            vacationStart: {
+                date: true,
+                required: true
+            },
+            vacationEnd: {
+                date: true,
+                required: true
+            }
+        },
+        messages: {
+            vacationType: {
+                required: 'أدخل نوع الإجازة!'
+            },
+            vacationStart: {
+                date: 'أدخل تاريخ مناسب',
+                required: 'هذا الحقل ضروري'
+            },
+            vacationEnd: {
+                date: 'أدخل تاريخ مناسب',
+                required: 'هذا الحقل ضروري'
             }
         }
     });
-    
+
+//    $(".selector2").validate({
+//        success: function (label) {
+//            label.text("ok!").parent().addClass("has-success");
+//        },
+//        error: function (label) {
+//            label.text("ok!").parent().addClass("has-error");
+//        },
+//        rules: {
+//            // at least 15€ when bonus material is included
+//            pay_what_you_want: {
+//                required: true
+//                min: {
+//                    // min needs a parameter passed to it
+//                    param: 15,
+//                    depends: function (element) {
+//                        return $("#bonus-material").is(":checked");
+//                    }
+//                }
+//            }
+//        }
+//    });
+
     //Vacation Credit
-    
+
     $("#sparkline7").sparkline([52, 12, 44], {
         type: 'pie',
         height: '150px',
-        sliceColors: ['#1ab394', '#b3b3b3', '#e4f0fb']});
+        sliceColors: ['#1ab394', '#b3b3b3', '#e4f0fb']
+    });
+
+    $(".select2").select2({
+        dir: "rtl"
+    });
 
 });
