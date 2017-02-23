8a71934619312e96262b7ffa00dc509403d75877
Amine <hamrouni.amin@gmail.com>
2017-02-23 15:00:16 +0300

    add style

--- js/pages/inquiry.js
+++ js/pages/inquiry.js
@@ -68,9 +68,6 @@ $(document).ready(function () {
         }
     }).validate({
         rules: {
-            confirm: {
-                equalTo: "#password"
-            },
             vacationType: {
                 required: true
             },
@@ -79,6 +76,10 @@ $(document).ready(function () {
                 required: true
             },
             vacationEnd: {
+                date: true,
+                required: true
+            },
+            vacationDuration: {
                 date: true,
                 required: true
             }
@@ -105,29 +106,26 @@ $(document).ready(function () {
 
     //Vacation Object
     var vacation = {
-        totalCredit: 120,
-        inProgressCredit: 10,
-        oldCredit: 20,
+        totalCredit: 90,
+        inProgressCredit: 12,
+        oldCredit: 15,
         newCredit: 0,
         restCredit: function () {
             return this.totalCredit - this.oldCredit - this.newCredit - this.inProgressCredit;
         },
-        type: 0,
-        startDate: 0,
-        endDate: 0
+        sourceCredit: 0
     };
 
-    var elVacationStart = document.getElementById("vacationStart");
-    var elVacationEnd = document.getElementById("vacationEnd");
-
-    vacation.startDate = elVacationStart.value;
-    vacation.endDate = elVacationEnd.value;
-
     //Init Vacation Credit Chart
-    $("#vacationChart").sparkline([vacation.restCredit(), vacation.oldCredit, vacation.inProgressCredit, vacation.newCredit], {
+    $("#vacationChart").sparkline([
+        vacation.newCredit,
+        vacation.restCredit(), 
+        vacation.oldCredit, 
+        vacation.inProgressCredit                
+    ], {
         type: 'pie',
         height: '150px',
-        sliceColors: ['#e8ecff', '#23c6c8', '#1c84c6', '#f8ac59', '#ED5565']
+        sliceColors: ['#23c6c8', '#e8ecff', '#f8ac59', '#1c84c6', '#ED5565']
     });
 
     //Vacation Labels
@@ -142,9 +140,10 @@ $(document).ready(function () {
     //TouchSpin number of days
     var elvacationDuration = $("input[name='vacationDuration']"),
         lblNewVacation;
-
+    // Init the TouchSpin element = disabled
     elvacationDuration.prop("disabled", true);
 
+    //Init the TouchSpin element
     elvacationDuration.TouchSpin({
         verticalbuttons: true,
         initval: 0,
@@ -161,31 +160,38 @@ $(document).ready(function () {
     lblNewVacation.html($('#vacationDuration').val());
 
     // on hanging vacationType:
-    // 1 - change the max/min number of days
-    // 2 - change the help message
+    //// 1 - change the max/min number of days
+    /// 2 - change the help message
+    //// 3 - update the labels containg the value of the vacation parametre
+    /// 4 - update vacation chart
 
     var helpMsg, elVacationType, maxVal, minVal, initValD;
 
     $("#vacationType").change(function () {
 
+        //Get the value of the vacation type
         elVacationType = $("#vacationType").val();
 
+        //the first type اجازة عادية
         if (elVacationType == 1) {
             helpMsg = 'عدد الأيام لا يمكن أن يكون أقل من 5 أيام';
             elvacationDuration.prop("disabled", false);
             initValD = 5;
             minVal = 5;
-            maxVal = 36;
+            maxVal = vacation.restCredit();
         }
 
+        //the second type اجازة اضطرارية
         if (elVacationType == 2) {
             helpMsg = 'عدد الأيام لا يمكن أن يكون أكثر من 5 أيام';
             elvacationDuration.prop("disabled", false);
+            elvacationDuration.val("1");
             initValD = 1;
             minVal = 1;
             maxVal = 5;
         }
 
+        //if there is no selection
         if (elVacationType == 0) {
             helpMsg = 'نرجو إختيار نوع الإجازة';
             $("input[name='vacationDuration']").show().prop("disabled", true);
@@ -194,37 +200,85 @@ $(document).ready(function () {
             maxVal = 0;
         }
 
+        //Update the touchspin
         elvacationDuration.trigger("touchspin.updatesettings", {
             max: maxVal,
             min: minVal,
             initval: initValD
         });
 
+        //Update the label containing the new vacation
         lblNewVacation.html($('#vacationDuration').val());
+
+        //Update the value of 'newCredit' in 'vacation'
         vacation.newCredit = $('#vacationDuration').val();
-        
+
         //Init Vacation Credit Chart
-        $("#vacationChart").sparkline([vacation.restCredit(), vacation.oldCredit, vacation.inProgressCredit, vacation.newCredit], {
+        $("#vacationChart").sparkline([
+            vacation.newCredit,
+            vacation.restCredit(), 
+            vacation.oldCredit, 
+            vacation.inProgressCredit                
+        ], {
             type: 'pie',
             height: '150px',
-            sliceColors: ['#e8ecff', '#23c6c8', '#1c84c6', '#f8ac59', '#ED5565']
+            sliceColors: ['#23c6c8', '#e8ecff', '#f8ac59', '#1c84c6', '#ED5565']
         });
 
+        //Update the vacation chart
         $("input[name='vacationDuration']").change(function () {
             lblNewVacation.html($('#vacationDuration').val());
             vacation.newCredit = $('#vacationDuration').val();
-            console.log(vacation.newCredit);
+
             //Init Vacation Credit Chart
-            $("#vacationChart").sparkline([vacation.restCredit(), vacation.oldCredit, vacation.inProgressCredit, vacation.newCredit], {
+            $("#vacationChart").sparkline([
+                vacation.newCredit,
+                vacation.restCredit(), 
+                vacation.oldCredit, 
+                vacation.inProgressCredit                
+            ], {
                 type: 'pie',
                 height: '150px',
-                sliceColors: ['#e8ecff', '#23c6c8', '#1c84c6', '#f8ac59', '#ED5565']
+                sliceColors: ['#23c6c8', '#e8ecff', '#f8ac59', '#1c84c6', '#ED5565']
             });
 
         });
 
+    });
 
+    /*\
+    ***
+    ***
+    ***
+    \*/
 
+    //*****Switch Button for the credit type
+    var typeSwitch = document.querySelector('.typeSwitch');
+    
+    var switchery_3 = new Switchery(typeSwitch, {
+        color: 'rgb(27, 175, 153)',
+        secondaryColor: 'rgb(27, 175, 153)',
+        jackColor: '#fcf45e',
+        jackSecondaryColor: '#c8ff77',
+        size: 'small'
+    });
+
+    var clickCheckbox = document.querySelector('.typeSwitch');
+    
+    clickCheckbox.addEventListener('change', function() {
+        if (clickCheckbox.checked) {
+            vacation.sourceCredit = true;
+            console.log(vacation.sourceCredit);
+        } else {
+            vacation.sourceCredit = false;
+            console.log(vacation.sourceCredit);
+        }
+    });
+    
+    //*******End Switch Button for the credit type
+
+    $('#vacationStart').calendarsPicker({
+        calendar: $.calendars.instance('islamic')
     });
 
 
